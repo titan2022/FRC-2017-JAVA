a8cec6a3dae1ec96dc2036f6baf58393060f88ce
DhvanilPopat <dpopat99@gmail.com>
2017-01-12 09:40:49 -0600

    Revised

--- Robot/src/org/usfirst/frc/team2022/robot/Robot.java
+++ Robot/src/org/usfirst/frc/team2022/robot/Robot.java
@@ -43,28 +43,8 @@ public class Robot extends IterativeRobot {
     	
     	//Instantiate Commands
     	driveCommand = new DriveCommand();
-
-  
-    	//thread for Camera Server
-    	Thread thread = new Thread() {
-    		public void run(){
-    			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-                camera.setResolution(640, 480);
-                
-                CvSink cvSink = CameraServer.getInstance().getVideo();
-                CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
-                
-                Mat source = new Mat();
-                Mat output = new Mat();
-                
-                while(true) {
-                    cvSink.grabFrame(source);
-                    //Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
-                    outputStream.putFrame(output);
-                } 
-    		}	
-    	};
-    		thread.start();  
+    	
+    	CameraServer.getInstance().startAutomaticCapture();
     }
     
 	
