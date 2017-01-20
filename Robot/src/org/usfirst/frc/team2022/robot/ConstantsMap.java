8b39be1ea61c82a320b0e295c004f001b34aa6c4
Michael Dow <mdow@imsa.edu>
2017-01-19 19:22:40 -0600

    AutoDriveStraight Command

--- Robot/src/org/usfirst/frc/team2022/robot/ConstantsMap.java
+++ Robot/src/org/usfirst/frc/team2022/robot/ConstantsMap.java
@@ -6,9 +6,20 @@ package org.usfirst.frc.team2022.robot;
 
 public class ConstantsMap {
 
-	public static final double DRIVE_ENCODER_DIST_PER_TICK = 0;
-	public static double kP = 0;
-	public static double kI = 0; 
+	public static final double DRIVE_ENCODER_DIST_PER_TICK_LEFT = ((4* Math.PI)/(360)*1.5);
+	public static final double DRIVE_ENCODER_DIST_PER_TICK_RIGHT = ((4* Math.PI)/(500)*1.5);
+
+	
+	public static final double motorSpeed = 0; 
+
+	public static double kP = 0.3;
+	public static double kI = 0;
 	public static double kD = 0; 
-	public static double kF = 0; 
+	public static double kF = 0;
+	
+	public static double P = 0.01;
+	public static double I = 0; 
+	public static double D = 0; 
+	public static double F = 0;
+
 }
